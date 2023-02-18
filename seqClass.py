4cb500c818213fc30cbe2a7b3397f6baa2efab78
Laura Escudero <laura.escudero@uvic.cat>
2023-02-18 16:05:03 -0500

    The script was edited,additional comments were added

--- seqClass.py
+++ seqClass.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# Import packages and functions required
 import sys, re
 from argparse import ArgumentParser
 
@@ -8,19 +9,21 @@ parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
-
+# If the length of the input sequence is 1, exits the script
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 # Identifies whether the sequence is DNA or RNA
+# The 'if not' is added to avoid classifying a sequence that contains T and U as DNA or RNA
 args = parser.parse_args()
-args.seq = args.seq.upper() 
+args.seq = args.seq.upper() # Transforms the sequence in upper case
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
-# The 'if not' is adding to avoig classifying a sequence that contains T and U as DNA
         if not re.search('U', args.seq):
             print ('The sequence is DNA')
+        else:
+            print ('Please check the sequence, it contains both T and U')
     elif re.search('U', args.seq):
         print ('The sequence is RNA')
     else:
